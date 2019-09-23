e5112b890b5e7e6eabc8dbf80566ef656c6343ec
maximmasterr <mrom06@ya.ru>
2019-09-23 13:23:34 +0000

    Deploying to gh-pages from master 42833c156a0f05f7f5bacebf5802b837bdeecb9c

---
+++ api/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"helloWorld","url":"globals.html#helloworld","classes":"tsd-kind-function"}]};
